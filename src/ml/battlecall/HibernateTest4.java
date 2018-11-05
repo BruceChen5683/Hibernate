671d807811d606da074d5bd6b1cea7749da35662
BruceChen5683 <BruceChen_foreign@126.com>
2018-11-06 07:48:13 +0800

    延迟加载以及立即加载

--- src/ml/battlecall/HibernateTest4.java
+++ src/ml/battlecall/HibernateTest4.java
@@ -12,7 +12,7 @@ import org.hibernate.Transaction;
 public class HibernateTest4 {
     public static void main(String[] args) {
 
-        Student student = new Student();
+        /*Student student = new Student();
         student.setName("test");
 
         IDCard idCard = new IDCard();
@@ -35,6 +35,64 @@ public class HibernateTest4 {
             }
         }finally {
             HibernateUtils.close(session);
+        }*/
+
+
+        /**
+         * fetch 属性 select or join
+         *
+         *
+         *              ******select:******
+         *
+         * Hibernate: select student0_.ID as ID1_1_0_, student0_.NAME as NAME2_1_0_ from student student0_ where student0_.ID=?
+         * Hibernate: select idcard0_.ID as ID1_0_0_, idcard0_.NUMBER as NUMBER2_0_0_, student1_.ID as ID1_1_1_, student1_.NAME as NAME2_1_1_ from idcard idcard0_ left outer join student student1_ on idcard0_.ID=student1_.ID where idcard0_.ID=?
+         * HibernateTest4.main 123
+         *
+         *
+         *              ******join:*****
+         * Hibernate: select student0_.ID as ID1_1_0_, student0_.NAME as NAME2_1_0_, idcard1_.ID as ID1_0_1_, idcard1_.NUMBER as NUMBER2_0_1_ from student student0_ left outer join idcard idcard1_ on student0_.ID=idcard1_.ID where student0_.ID=?
+         * HibernateTest4.main 123
+         * */
+        /*Session session = HibernateUtils.openSession();
+        Transaction transaction = null;
+
+        try{
+            transaction = session.beginTransaction();
+            Student student = session.get(Student.class,"402881ed66e1344d0166e1344fdb0000");
+            System.out.println("HibernateTest4.main "+ student.getIdCard().getNumber());
+            transaction.commit();
+
+        }catch (Exception e){
+            if (null != transaction){
+                transaction.rollback();
+            }
+        }finally {
+            HibernateUtils.close(session);
+        }/
+
+
+        /**
+         * 延迟加载
+         * */
+
+        Session session = HibernateUtils.openSession();
+        Transaction transaction = null;
+        Student student = null;
+
+        try{
+            transaction = session.beginTransaction();
+            student = session.get(Student.class,"402881ed66e1344d0166e1344fdb0000");
+            transaction.commit();
+
+        }catch (Exception e){
+            if (null != transaction){
+                transaction.rollback();
+            }
+        }finally {
+            HibernateUtils.close(session);
+
+            System.out.println("HibernateTest4.main "+ student.getIdCard().getNumber());
+
         }
     }
 }
