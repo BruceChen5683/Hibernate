22d3e40ef730619cad8f92917ba4ed8a92c9c3ba
cjl <605385881@qq.com>
2018-11-07 21:57:33 +0800

    constrained

--- src/ml/battlecall/util/HibernateUtils.java
+++ src/ml/battlecall/util/HibernateUtils.java
@@ -4,6 +4,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.io.File;
+
 /**
  * Created by chenjianliang on 2018/5/29.
  */
