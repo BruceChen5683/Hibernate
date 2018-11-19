c40c162fbad801294e40c42cc47080ed716764f5
cjl <605385881@qq.com>
2018-11-20 07:27:53 +0800

    自定义排序

---
+++ src/ml/battlecall/MyComparator.java
@@ -0,0 +1,12 @@
+package ml.battlecall;
+
+import ml.battlecall.hibernateMap.TStudent;
+
+import java.util.Comparator;
+
+public class MyComparator implements Comparator<TStudent> {
+    @Override
+    public int compare(TStudent tStudent, TStudent t1) {
+        return 0;
+    }
+}
