41312dc0baf5a54a1cfd20060ae4c14ea3808927
cjl <605385881@qq.com>
2018-11-20 06:58:14 +0800

    数据库排序 内存排序

--- src/ml/battlecall/HibernateTestMap.java
+++ src/ml/battlecall/HibernateTestMap.java
@@ -21,22 +21,25 @@ public class HibernateTestMap {
             /**
              * save
              *
-             *             Team2 team2 = new Team2();
-             *             team2.setTeamName("team");
-             *
-             *             Map map = team2.getStudents();
-             *             map.put("zhangsan","hi");
-             *             map.put("lisi","haha");
-             *             map.put("cc","haha");
-             *             map.put("aa","haha");
              *
              *
-             *             session.save(team2);
              */
+//                        Team2 team2 = new Team2();
+//                          team2.setTeamName("team");
+//
+//                          Map map = team2.getStudents();
+//                          map.put("zhangsan","hi");
+//                          map.put("lisi","haha");
+//                          map.put("cc","haha");
+//                          map.put("bb","haha");
+//
+//
+//                          session.save(team2);
+
 
 
             /**
-             * query
+             * query get a uniqueResult
              */
 
             Team2 team2 = (Team2) session.createQuery("from Team2 t where t.teamName = 'team'").uniqueResult();
@@ -49,6 +52,26 @@ public class HibernateTestMap {
 
                 System.out.println("HibernateTestMap.main  "+                iterator.next());
             }
+
+
+//            /**
+//             * query get a list
+//             */
+//
+//            for (Object team2 :session.createQuery("from Team2 t where t.teamName = 'team'").list()){
+//                Map map = ((Team2)team2).getStudents();
+//
+//                Collection collection = map.keySet();
+//
+//                Iterator iterator = collection.iterator();
+//                while (iterator.hasNext()){
+//
+//                    System.out.println("HibernateTestMap.main  "+                iterator.next());
+//                }
+//
+//            }
+
+
 
             transaction.commit();
 
